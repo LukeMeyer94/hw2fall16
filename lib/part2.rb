305a218419861286b900e7f3b1709abff995e021
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-09-12 23:17:22 +0000

    part 2 started

--- lib/part2.rb
+++ lib/part2.rb
@@ -3,7 +3,45 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
-  # YOUR CODE HERE
+  game[0][1] = game[0][1].upcase
+  game[1][1] = game[1][1].upcase
+  winner = compareMoves(game[0][1], game[1][1])
+  if winner == 2
+    return game[1]
+  else
+    return game[0]
+  end
+end
+
+def compareMoves(playerOne, playerTwo)
+  case playerOne
+  when "R"
+    if playerTwo == "R" || playerTwo == "S"
+      return 1
+    elsif playerTwo == "P"
+      return 2
+    else
+      raise NoSuchStrategyError
+    end
+  when "P"
+    if playerTwo == "P" || playerTwo == "R"
+      return 1
+    elsif playerTwo == "S"
+      return 2
+    else
+      raise NoSuchStrategyError
+    end
+  when "S"
+    if playerTwo == "S" || playerTwo == "P"
+      return 1
+    elsif playerTwo == "R"
+      return 2
+    else
+      raise NoSuchStrategyError
+    end
+  else
+    raise NoSuchStrategyError
+  end
 end
 
 def rps_tournament_winner(tournament)
--- spec/part2_spec.rb
+++ spec/part2_spec.rb
@@ -10,6 +10,11 @@ describe "#rps_game_winner" do
 #    lambda { rps_game_winner([ ["Allen", "S"] ]) }.should raise_error, "No error raised for incorrect number of players"
     expect(lambda { rps_game_winner([ ["Allen", "S"] ]) }).to raise_error(WrongNumberOfPlayersError)
   end
+  
+  it "should output the name of the winner" do 
+    expect(rps_game_winner([["Winner", "R"], ["Loser", "S"]])).to be == ["Winner", "R"]
+    expect(rps_game_winner([["Loser", "p"], ["Winner", "s"]])).to be == ["Winner", "S"]
+  end
 end
 
 describe "#rps_tournament_winner" do
@@ -17,4 +22,19 @@ describe "#rps_tournament_winner" do
 #    lambda { rps_tournament_winner([["X", "P"], ["Y", "S"]]) }.should_not raise_error
     expect(lambda { rps_tournament_winner([["X", "P"], ["Y", "S"]]) }).not_to raise_error
   end
+  it "should return the correct winner of a tournament" do
+    expect(rps_tournament_winner(
+      [
+        [
+          [ ["Joe", "P"], ["Mary", "S"] ],
+          [ ["Bob", "R"], ["Alice", "S"] ]
+        ],
+        [
+          [ ["Steve", "S"], ["Jane", "P"] ],
+          [ ["Ted", "R"], ["Carol", "P"] ]
+        ]
+      ]
+    )).to be == ["Bob", "R"]
+    end
+
 end
