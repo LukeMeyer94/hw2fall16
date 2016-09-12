2ef44a7fb2eb35f6f0470dc9a4a182f2129e92a0
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-09-12 23:17:29 +0000

    part 3

--- lib/part3.rb
+++ lib/part3.rb
@@ -1,24 +1,40 @@
 class Dessert
+  
+  attr_accessor :name
+  attr_accessor :calories
 
   def initialize(name, calories)
-    # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
   def healthy?
-    # YOUR CODE HERE
+    if @calories.to_i < 200
+      return true
+    else
+      return false
+    end
   end
   
   def delicious?
-    # YOUR CODE HERE
+    return true
   end
 end
 
 class JellyBean < Dessert
+  
+  attr_accessor :flavor
+  
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    super(name, calories)
+    @flavor = flavor
   end
   
   def delicious?
-    # YOUR CODE HERE
+    if @flavor.downcase == "black licorice"
+      return false
+    else
+      return true
+    end
   end
 end
--- spec/part3_spec.rb
+++ spec/part3_spec.rb
@@ -5,11 +5,18 @@ describe "dessert" do
 #    lambda { Dessert.new('a', 1) }.should_not raise_error
     expect(lambda {Dessert.new('a', 1)}).not_to raise_error
   end
-  %w(:healthy? :delicious?).each do |method|
+  %i(healthy? delicious?).each do |method|
     it "should define #{method}" do
-#      Dessert.new('a',1).should respond_to method
+#     Dessert.new('a',1).should respond_to method
       expect(Dessert.new('a',1)).to respond_to(method)
     end
+  end
+  it "should return true when asked if a dessert is delicious" do
+    expect(Dessert.new("Something",398).delicious?).to be true
+  end
+  it "should return true when asked if a dessert is healthy only if that dessert is under 200 calories" do
+    expect(Dessert.new("Something",398).healthy?).to be false
+    expect(Dessert.new("Something",198).healthy?).to be true
   end
 end
 
@@ -18,10 +25,20 @@ describe "jellybean" do
 #    lambda { JellyBean.new('a', 1, 2) }.should_not raise_error
     expect(lambda { JellyBean.new('a', 1, 2) }).not_to raise_error
   end
-  %w(:healthy? :delicious? ).each do |method|
+  %i(healthy? delicious?).each do |method|
     it "should define #{method}" do
 #      JellyBean.new('a',1, 2).should respond_to method 
       expect(JellyBean.new('a',1, 2)).to respond_to(method) 
     end
+  end
+  it "should return false when asked if black licorice jellybeans are delicious" do
+    expect(JellyBean.new('jb',1, "Black Licorice").delicious?).to be false
+  end
+  it "should return true when asked if any other flacors are delicious" do
+    expect(JellyBean.new('jb',1, "cherry").delicious?).to be true
+  end
+  it "should correctly implement parent method healthy" do
+    expect(JellyBean.new("Something",398,"strawb").healthy?).to be false
+    expect(JellyBean.new("Something",198,"blueb").healthy?).to be true
   end
 end
