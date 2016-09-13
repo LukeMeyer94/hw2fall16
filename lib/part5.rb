9e663fe7e4f35fae098ff668c89315dda8756028
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-09-13 00:44:05 +0000

    part 5

--- lib/part5.rb
+++ lib/part5.rb
@@ -1,6 +1,34 @@
 class CartesianProduct
   include Enumerable
+  
+  attr_accessor :product
 
-  # YOUR CODE HERE
+  def initialize(a,b)
+    @a = a
+    @b = b
+    calculateProduct
+  end
+  
+  def calculateProduct
+    @product = Array.new
+    for i in 0...@a.length
+      @innerProduct = Array.new
+      for j in 0...@b.length
+        @innerProduct[j] = [@a[i],@b[j]]
+      end
+      @product[i] = @innerProduct
+    end
+  end
+  
+  def each(&block)
+    @product.each do |p|
+      p.each do |iP|
+        block.call(iP)
+      end
+    end
+  end
 
 end
+
+# c = CartesianProduct.new([:a,:b],[4,5])
+# c.each { |elt| puts elt.inspect}
--- spec/part5_spec.rb
+++ spec/part5_spec.rb
@@ -2,8 +2,11 @@ require 'part5.rb'
 
 describe "CartesianProduct" do
   it "should exist" do
-#    lambda { CartesianProduct.new(1..2,3..4) }.should_not raise_error
-    expect(lambda { CartesianProduct.new(1..2,3..4) }).not_to raise_error
+    expect(lambda { CartesianProduct.new([:a,:b],[4,5]) }).not_to raise_error
+  end
+  it "should form an appropriate array" do
+    c = CartesianProduct.new([:a,:b],[4,5,6])
+    expect(c.product).to be == [[[:a,4],[:a,5],[:a,6]],[[:b,4],[:b,5],[:b,6]]]
   end
 end
 
