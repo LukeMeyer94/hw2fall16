35902e6253ade26e16ab0c90b4c98dec7a1db0ff
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-09-13 13:22:32 +0000

    added attr_accessor_with_history for class Class (part4)

--- lib/part4.rb
+++ lib/part4.rb
@@ -1,16 +1,22 @@
 class Class
-  
-  attr_accessor :bar
-  attr_accessor :foo
-  
-  def initialize
-  end
 
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "YOUR CODE HERE, USE %Q FOR MULTILINE STRINGS"
+    class_eval %Q"
+      def #{attr_name}=(new_val)
+        @#{attr_name}_history || @#{attr_name}_history = [nil]
+        @#{attr_name}_history << @#{attr_name} = new_val
+      end
+      def #{attr_name}_history
+        return @#{attr_name}_history
+      end
+    "
 
   end
+end
+
+class Foo
+  attr_accessor_with_history :bar
 end
