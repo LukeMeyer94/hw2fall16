b42a781b1a8d75dac33a9eb26729e78a0edcc6eb
Luke Meyer <luke-r-meyer@uiowa.edu>
2016-09-13 13:21:57 +0000

    added additional test for part4

--- spec/part4_spec.rb
+++ spec/part4_spec.rb
@@ -5,5 +5,13 @@ describe "Class" do
 #    lambda { Class.new.attr_accessor_with_history :x }.should_not raise_error
     expect(lambda { Class.new.attr_accessor_with_history :x }).not_to raise_error
   end
+  
+  it "should work correctly with class Foo" do
+    f = Foo.new
+    f.bar = 1
+    f.bar = 2
+    f.bar = 3
+    expect(f.bar_history.to_s).to be == [nil, 1, 2, 3].to_s
+  end
 end
 
